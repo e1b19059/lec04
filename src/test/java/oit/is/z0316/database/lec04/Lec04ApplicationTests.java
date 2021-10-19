4cf0a7c74975e03243213fe0c747dc9145c98063
e1b19059 <e1b19059@oit.ac.jp>
2021-10-19 14:14:47 +0900

    リポジトリ初期化

---
+++ src/test/java/oit/is/z0316/database/lec04/Lec04ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0316.database.lec04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
